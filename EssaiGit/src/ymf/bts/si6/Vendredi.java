1d431a064d24e26a327399045d4fa4b1b9ccd4c8
yfontenoy <yfontenoy@B4-prof.btssio.fr>
2018-02-09 15:36:12 +0100

    Version 1.0

---
+++ EssaiGit/src/ymf/bts/si6/Vendredi.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ymf.bts.si6;
+
+/**
+ * @author yfontenoy
+ *
+ */
+public class Vendredi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Bonjour Ca Va ??");
+	}
+
+}
